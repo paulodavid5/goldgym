22545dbdbbfa7aa499374bd22920346ae5916448
paulodavid5 <paulodaviddias4@gmail.com>
2022-07-22 15:17:35 +0100

    3 commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
 const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
+const root = createRoot(rootElement);
 
 root.render(
     <BrowserRouter>
